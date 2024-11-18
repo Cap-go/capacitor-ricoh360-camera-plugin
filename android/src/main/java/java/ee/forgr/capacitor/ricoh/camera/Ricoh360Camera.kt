e67bd059278e3388b01a0b27bf5fe2fae3f729cf
WcaleNieWolny <isupermichael007@gmail.com>
2024-11-18 05:45:16 +0100

    feat: kotlin

---
+++ android/src/main/java/java/ee/forgr/capacitor/ricoh/camera/Ricoh360Camera.kt
@@ -0,0 +1,10 @@
+package java.ee.forgr.capacitor.ricoh.camera
+
+import android.util.Log
+
+class Ricoh360Camera {
+    fun echo(value: String?): String? {
+        Log.i("Echo", value!!)
+        return value
+    }
+}
