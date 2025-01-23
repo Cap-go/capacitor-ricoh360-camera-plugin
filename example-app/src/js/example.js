16edf7d4a33ac4ac03692b3b4e6b3255668c5dcc
Martin Donadieu <martindonadieu@gmail.com>
2025-01-23 12:27:42 +0000

    fix: build

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,6 +1,14 @@
 import { Ricoh360Camera } from 'richon360-camera';
 
+async function getCameraSizes() {
+    const sizes =  await Ricoh360Camera.initialize({ language: 'en-US', cameraUrl: 'http://192.168.1.1:3000' })  // await CameraPreview.getSupportedPictureSizes()
+    console.log(sizes)
+    await Ricoh360Camera.livePreview()
+  }
+
 window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    Ricoh360Camera.echo({ value: inputValue })
+    // const inputValue = document.getElementById("echoInput").value;
+    // Ricoh360Camera.echo({ value: inputValue })
+    getCameraSizes()
 }
+
