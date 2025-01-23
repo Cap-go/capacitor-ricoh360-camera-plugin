1bb13d1b364f2f08f9bc49e4d62d356e146f7508
WcaleNieWolny <isupermichael007@gmail.com>
2025-01-23 15:51:12 +0100

    feat: changes

--- Package.swift
+++ Package.swift
@@ -10,19 +10,24 @@ let package = Package(
             targets: ["Ricoh360CameraPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "Ricoh360CameraPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "THETAClient")
             ],
             path: "ios/Sources/Ricoh360CameraPlugin"),
         .testTarget(
             name: "Ricoh360CameraPluginTests",
             dependencies: ["Ricoh360CameraPlugin"],
             path: "ios/Tests/Ricoh360CameraPluginTests")
+        .binaryTarget(
+            name: "THETAClient",
+            path: "submodules/theta-client/kotlin-multiplatform/build/cocoapods/publish/release/THETAClient.xcframework",
+        )
     ]
 )
