da99c7f2e737221276630e9e1271dbcc5f95f5c2
Martin Donadieu <martindonadieu@gmail.com>
2025-01-23 12:29:00 +0000

    simple test

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,12 +1,20 @@
 import { Ricoh360Camera } from 'richon360-camera';
 
 async function getCameraSizes() {
-    const sizes =  await Ricoh360Camera.initialize({ language: 'en-US', cameraUrl: 'http://192.168.1.1:3000' })  // await CameraPreview.getSupportedPictureSizes()
+    const sizes =  await Ricoh360Camera
+    .initialize({ language: 'en-US', cameraUrl: 'http://192.168.1.1:3000' })
+    .catch(e => {
+        console.log(e)
+    })
     console.log(sizes)
-    await Ricoh360Camera.livePreview()
+    await Ricoh360Camera
+    .livePreview()
+    .catch(e => {
+        console.log(e)
+    })
   }
 
-window.testEcho = () => {
+window.testCamera = () => {
     // const inputValue = document.getElementById("echoInput").value;
     // Ricoh360Camera.echo({ value: inputValue })
     getCameraSizes()
