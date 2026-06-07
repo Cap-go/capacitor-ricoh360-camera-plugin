6454d5e59b665af4fc234f426d6e620c9cfbecc6
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:44 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,22 +3,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.ricoh360.camera.plugin",
-  "appName": "Ricoh360 Camera Plugin Example",
-  "webDir": "dist",
-  "plugins": {
-    "SplashScreen": {
-      "launchShowDuration": 0
+  appId: 'app.capgo.ricoh360.camera.plugin',
+  appName: 'Ricoh360 Camera Plugin Example',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
     },
-    "CapacitorUpdater": {
-      "appId": "app.capgo.ricoh360.camera.plugin",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+    CapacitorUpdater: {
+      appId: 'app.capgo.ricoh360.camera.plugin',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
